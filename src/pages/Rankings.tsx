02184d375c00a4cbc3e8d4b0fd23669d84fa2a14
Matheus Bernardes <matheusbnas@gmail.com>
2025-06-26 22:50:17 -0300

    Create missing Rankings component

---
+++ src/pages/Rankings.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Rankings: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-6">🏆 Rankings</h1>
+      <div className="bg-white rounded-lg shadow-md p-6">
+        <p className="text-gray-600">Rankings functionality will be implemented here.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Rankings;
